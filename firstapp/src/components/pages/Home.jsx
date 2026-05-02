176ad875723a06930e26b67bc01b8536b2f44c18
kaifAshraf-202 <kaifashraf027@gmail.com>
2026-05-02 21:15:05 +0530

    card

--- firstapp/src/components/pages/Home.jsx
+++ firstapp/src/components/pages/Home.jsx
@@ -1,9 +1,29 @@
+import "./Home.css";
+
 function Home() {
   return (
     <div className="home">
       <h1>Welcome to the Home Page!</h1>
       <p>This is the main landing page of our application.</p>
+      <section className="cardmid">
+        <h2>our cards</h2>
+        <section className="card">
+         <card />
+         <card />
+         <card />
+         <card />
+         <card />
+        </section>
+      </section>
     </div>
   );
 }
 export default Home;
+function card() {
+  return (
+    <>
+   <div>card</div> 
+    
+    </>
+    
+  )}
--- firstapp/src/main.jsx
+++ firstapp/src/main.jsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import Home from './components/pages/Home.jsx'
+import { Footer } from './components/common/Footer.jsx'
+import { Header } from './components/common/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <>
+  <Header />
   <Home />
+  <Footer />
+  </>
 )
